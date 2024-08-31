1b9061c7882ce46d0d70b2c697c012fc1c00e8b3
pedroaal <pedroaal04@gmail.com>
2024-08-31 08:59:15 -0500

    dummy actions

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,44 @@
+import useAlertStore from "~/store/alertStore"
+import useLoaderStore from "~/store/loaderStore"
+import useModalStore from "~/store/modalStore"
+
 const Home = () => {
-  return <div>Home</div>
+  const { addAlert, removeAlert } = useAlertStore()
+  const { addLoader, removeLoader } = useLoaderStore()
+  const { openModal } = useModalStore()
+
+  const handleModal = () => {
+    openModal({ component: "main", props: { title: "Main Modal" } })
+  }
+
+  const handleAlert = () => {
+    addAlert({ title: "Alert", type: "SUCCESS", value: "Success" })
+    setTimeout(() => {
+      removeAlert({ title: "Alert", type: "SUCCESS", value: "Success" })
+    }, 1000)
+  }
+
+  const handleLoader = () => {
+    addLoader("main")
+    setTimeout(() => {
+      removeLoader("main")
+    }, 1000)
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h4>Home</h4>
+      <button type="button" className="btn" onClick={handleModal}>
+        Open Modal
+      </button>
+      <button type="button" className="btn" onClick={handleAlert}>
+        Add Alert
+      </button>
+      <button type="button" className="btn" onClick={handleLoader}>
+        Add Loader
+      </button>
+    </div>
+  )
 }
 
 export default Home
