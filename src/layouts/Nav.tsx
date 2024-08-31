9f3aaf5d1eaa7f7728edb657767cd6d2f746a0de
pedroaal <pedroaal04@gmail.com>
2024-08-31 08:55:50 -0500

    to location hook

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,21 +1,19 @@
-import { Link, useNavigation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 import navigation from "~/constants/navigation"
 
 const Nav = () => {
-  const navigator = useNavigation()
+  const location = useLocation()
 
   return (
     <div className="btm-nav">
       {navigation.map((route) => {
         const { icon } = route
-        const isActive = navigator.location === route.name ? "active" : ""
+        const isActive = location.pathname === route.name ? "active" : ""
 
         return (
-          <Link key={route.name} to={route.name}>
-            <button className={isActive}>
-              {icon({ className: "size-6" })}
-            </button>
+          <Link key={route.name} to={route.name} className={isActive}>
+            {icon({ className: "size-6" })}
           </Link>
         )
       })}
