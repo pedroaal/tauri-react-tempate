0efe31621b332b31d31feb10c2efa44d60a8a822
pedroaal <pedroaal04@gmail.com>
2024-08-30 20:41:25 -0500

    added portal

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,17 +1,26 @@
-import Alerts from "@/components/core/Alerts"
+import { createPortal } from "react-dom"
+import { Outlet } from "react-router-dom"
+
 import Nav from "./Nav"
-import Modals from "@/components/core/Modals"
-import Loader from "@/components/core/Loader"
+import Alerts from "~/components/core/Alerts"
+import Loader from "~/components/core/Loader"
+import Modals from "~/components/core/Modals"
 
 const Main = () => {
   return (
-    <div>
-      HOLA HOLA
-      <Alerts />
-      <Modals />
-      <Loader />
-      Main
-      <Nav />
+    <div className="min-w-screen min-h-screen">
+      <div className="container">
+        <Outlet />
+        <Nav />
+      </div>
+      {createPortal(
+        <>
+          <Alerts />
+          <Loader />
+          <Modals />
+        </>,
+        document.getElementById("portal") as HTMLElement,
+      )}
     </div>
   )
 }
