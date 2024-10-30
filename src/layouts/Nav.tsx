1c022b96b0aeb2ff035e3ed1bc295194098b6eab
pedroaal <pedroaal04@gmail.com>
2024-10-30 15:20:52 -0500

    fix navigation

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,19 +1,14 @@
-import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import navigation from "~/constants/navigation"
 
 const Nav = () => {
-  const location = useLocation()
-
   return (
-    <div className="btm-nav">
+    <div className="">
       {navigation.map((route) => {
-        const { icon } = route
-        const isActive = location.pathname === route.name ? "active" : ""
-
         return (
-          <Link key={route.name} to={route.name} className={isActive}>
-            {icon({ className: "size-6" })}
+          <Link key={route.name} to={route.name}>
+            {route.icon}
           </Link>
         )
       })}
