b6c6e4b38dd04f74a8857738afd82352d5d78d12
pedroaal <pedroaal04@gmail.com>
2024-10-30 16:57:07 -0500

    remove tanstank

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { createPortal } from "react-dom"
 import { RouterProvider } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import router from "./config/router"
 
@@ -9,17 +8,15 @@ import Loader from "~/components/core/Loader"
 
 import "./index.css"
 
-const queryClient = new QueryClient()
-
 const App = () => {
   const portal = document.getElementById("portal") as HTMLElement
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <RouterProvider router={router} />
       {createPortal(<Alerts />, portal)}
       {createPortal(<Loader />, portal)}
-    </QueryClientProvider>
+    </>
   )
 }
 
